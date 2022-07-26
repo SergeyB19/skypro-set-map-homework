8d0e5eecd7046875b3414b3c0c7545e9e2959233
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-07-26 09:09:25 +0300

    hw-set-map

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,56 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> words = new ArrayList<>(List.of("abc", "abc", "bcd", "bcd", "e"));
+
     public static void main(String[] args) {
-        //Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        List<Integer> numsOne = new ArrayList<>();
-        for (Integer num : nums) {
+        task1();
+        System.out.println();
+        task2();
+        System.out.println();
+        task3();
+        System.out.println();
+        task4();
+    }
+
+    //Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.
+    public static void task1() {
+        for (int num : nums) {
             if (num % 2 != 0) {
-                numsOne.add(num);
+                System.out.println(num);
             }
         }
-        System.out.println(numsOne);
+    }
 
-        //Напишите код, с помощью которого можно напечатать только четные числа в консоль.
-        List<Integer> numsEven = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        List<Integer> numsEvenOne = new ArrayList<>();
-        for (Integer num : nums) {
-            if (num % 2 == 0) {
-                numsEvenOne.add(num);
+    //Напишите код, с помощью которого можно напечатать только четные числа в консоль.
+    public static void task2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num;
             }
         }
-        System.out.println(numsEvenOne);
+    }
 
+    //Напишите код, который выводит в консоль все уникальные слова из списка слов, в котором могут встречаться дубли
+    public static void task3() {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(uniqueWords);
+    }
 
-        //Напишите код, который выводит в консоль все уникальные слова из списка слов, в котором могут встречаться дубли
-        Set<String> words = new LinkedHashSet<>(List.of("Bad boys", "Bad boys", "whatcha", "gonna", "do","gonna", "do"));
-        System.out.println(words);
-
-       // Напишите код, который выводит в консоль все количество дублей из списка слов.
-        List<String> words1 = new ArrayList<>(List.of("Bad boys", "Bad boys", "whatcha", "gonna", "do","chuck"));
-        Map<String, Integer> frequency = words1.stream().collect(Collectors.toMap(e,1, Integer::sum));
-
-        frequency.forEach((k, v) -> System.out.println(k + ": " + v));
-
+    // Напишите код, который выводит в консоль все количество дублей из списка слов.
+    public static void task4() {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(words.size() - uniqueWords.size());
     }
 }
+
+
+
+
 
 
 
